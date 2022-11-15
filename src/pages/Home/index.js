d9571a9d7800879013e3260dd9e4dd0ac98aa7f1
Fabricyo Ribeiro <fabricyosilva600@gmail.com>
2022-11-15 15:28:54 -0300

    finalized

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,45 +1,104 @@
+import { useState, useEffect } from 'react'
 import './home.css'
 
 import { Social } from '../../components/Social'
 
 import {FaFacebook, FaInstagram, FaYoutube} from 'react-icons/fa'
+import { db } from '../../services/fireBseConnection.js'
+
+import {
+  getDocs,
+  collection,
+  orderBy,
+  query,
+  doc,
+  getDoc
+} from 'firebase/firestore'
 
 export default function Home(){
+
+  const [links, setLinks] = useState([])
+  const [socialLinks, setSocialLinks] = useState({})
+
+  useEffect(() => {
+    function loadLinks(){
+      const linksRef = collection(db, 'links')
+      const queryRef = query(linksRef, orderBy('created', 'asc'))
+
+      getDocs(queryRef)
+      .then((snapshot) => {
+        let lista = []
+
+        snapshot.forEach((doc) => {
+          lista.push({
+            id: doc.id,
+             name: doc.data().name,
+             url: doc.data().url,
+             bg: doc.data().bg,
+             color: doc.data().color
+          })
+
+          setLinks(lista)
+        })
+      })
+    }
+    loadLinks()
+  }, [])
+
+  useEffect(() => {
+    function loadSocialLinks(){
+      const docRef = doc(db, 'social', 'link')
+
+      getDoc(docRef)
+      .then((snapshot) => {
+        if(snapshot.data() !== undefined){
+          setSocialLinks({
+            facebook: snapshot.data().facebook,
+            instagram: snapshot.data().instagram,
+            youtube: snapshot.data().youtube
+          })
+        }
+      })
+
+    }
+
+    loadSocialLinks()
+  }, [])
+
   return(
     <div className='home-container'>
       <h1>Fabricyo Ribeiro</h1>
       <span>Veja meus links 👇</span>
 
       <main className='links'>
-        <section className='link-area'>
-          <a href='#'>
-            <p className='link-text'>Canal no youtube</p>
+      
+      {
+        links.map((item) => (
+        <section key={item.id} className='link-area' style={{ backgroundColor: item.bg}}>
+          <a href={item.url} target='_blank'>
+            <p className='link-text' style={{ color: item.color}}>{item.name}</p>
           </a>
         </section>
-        <section className='link-area'>
-          <a href='#'>
-            <p className='link-text'>Canal no youtube</p>
-          </a>
-        </section>
-        <section className='link-area'>
-          <a href='#'>
-            <p className='link-text'>Canal no youtube</p>
-          </a>
-        </section>
+        ))
+      }
 
-        <footer>
-          <Social url='face'>
-            <FaFacebook size={35} color='#FFF' />
-          </Social>
-
-          <Social url='face'>
-            <FaInstagram size={35} color='#FFF' />
-          </Social>
-          
-          <Social url='face'>
-            <FaYoutube size={35} color='#FFF' />
-          </Social>
-        </footer>
+        {
+          links.length !== 0 && Object.keys(socialLinks).length > 0 && (
+            <footer>
+            <Social url={socialLinks?.facebook}>
+              <FaFacebook size={35} color='#FFF' />
+            </Social>
+  
+            <Social url={socialLinks?.instagram}>
+              <FaInstagram size={35} color='#FFF' />
+            </Social>
+            
+            <Social url={socialLinks?.youtube}>
+              <FaYoutube size={35} color='#FFF' />
+            </Social>
+          </footer>
+          )
+        }
       </main>
     </div>
   )
--- src/pages/Networks/index.js
+++ src/pages/Networks/index.js
@@ -1,29 +1,77 @@
 import './networks.css'
+import { useState, useEffect } from 'react'
 import { Header } from '../../components/Header'
 import { Input } from '../../components/Input'
 import { MdAddLink } from 'react-icons/md'
 
+import { db } from '../../services/fireBseConnection.js'
+import { setDoc, doc, getDoc } from 'firebase/firestore'
+
+import {toast} from 'react-toastify'
+
 export default function Networks(){
+  const [facebook, setFacebook] = useState('')
+  const [instagram, setInstagram] = useState('')
+  const [youtube, setYoutube] = useState('')
+
+  useEffect(() => {
+    function loadLinks(){
+      const docRef = doc(db, 'social', 'link')
+      getDoc(docRef)
+      .then((snapshot) => {
+        if(snapshot.data() !== undefined){
+          setFacebook(snapshot.data().facebook)
+          setInstagram(snapshot.data().instagram)
+          setYoutube(snapshot.data().youtube)
+        }
+      })
+    }
+
+    loadLinks()
+  }, [])
+
+  function handleSave(e){
+    e.preventDefault()
+    setDoc(doc(db, 'social', 'link'), {
+      facebook: facebook,
+      instagram: instagram,
+      youtube: youtube
+    })
+    .then(() => {
+      toast.success('Salvo com sucesso')
+    })
+    .catch((error) => {
+      toast.error('Erro ao salvar')
+    })
+  }
+
   return (
+
     <div className='admin-container'>
       <Header />
 
       <h1 className='title-social'>Suas redes sociais</h1>
 
-      <form className='form'>
+      <form className='form' onSubmit={handleSave}>
         <label>Link do Facebook</label>
         <Input 
           placeholder='Digite a url do facebook'
+          value={facebook}
+          onChange={ (e) => setFacebook(e.target.value)}
         />
 
         <label>Link do Instagram</label>
         <Input 
           placeholder='Digite a url do instagram'
+          value={instagram}
+          onChange={ (e) => setInstagram(e.target.value)}
         />
 
         <label>Link do Youtube</label>
         <Input 
           placeholder='Digite a url do youtube'
+          value={youtube}
+          onChange={ (e) => setYoutube(e.target.value)}
         />
 
         <button type='submit' className='btn-register'>
